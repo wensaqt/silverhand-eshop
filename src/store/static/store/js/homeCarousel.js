283c81ae30d325b9779e4042fa70a17c52aeb85b
wensaqt <116151112+wensaqt@users.noreply.github.com>
2023-06-07 10:50:08 +0200

    added a carousel for new items

--- src/store/static/store/js/homeCarousel.js
+++ src/store/static/store/js/homeCarousel.js
@@ -1,23 +1,2 @@
-class Carousel {
-    /**
-     * @param {HTMLElement} element 
-     * @param {Object} options 
-     * @param {Object} options.slidesToScroll --> nombre d'éléments à faire défiler
-     * @param {Object} options.slidesVisible --> nombre d'éléments visibles dans un slide
-     */
-    constructor(element, options = {}) {
-        this.element = element;
-        this.options = Object.assign({}, {
-            slidesToScroll: 1,
-            slidesVisible: 1
-        }, options)
-    }
-}
 
-document.addEventListener('DOMContentLoaded', function () {
-    new Carousel(document.querySelector('.news'), {
-        slidesToScroll: 3,
-        slidesVisible: 3
-    });
-});
 
